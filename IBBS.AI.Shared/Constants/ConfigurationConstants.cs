12d9074401897c20d95053c19149a84cfc3c678b
Debanjan Paul <debanjanpaul10@gmail.com>
2025-05-11 12:52:06 +0530

    Added azure configs and updated with auth0 authentication

--- IBBS.AI.Shared/Constants/ConfigurationConstants.cs
+++ IBBS.AI.Shared/Constants/ConfigurationConstants.cs
@@ -23,31 +23,6 @@ namespace IBBS.AI.Shared.Constants
         public const string AppConfigurationEndpointKeyConstant = "AppConfigurationEndpoint";
 
         /// <summary>
-        /// The gemini ai api url.
-        /// </summary>
-        public const string GeminiAIApiUrlConstant = "GeminiAIApiUrl";
-
-        /// <summary>
-        /// The bulletin ai token constant.
-        /// </summary>
-        public const string BulletinAITokenConstant = "BulletinAiToken";
-
-        /// <summary>
-        /// The bulletin ai client constant.
-        /// </summary>
-        public const string bulletinAiClientConstant = "bulletinAiClient";
-
-        /// <summary>
-        /// The application json constant
-        /// </summary>
-        public const string ApplicationJsonConstant = "application/json";
-
-        /// <summary>
-        /// The app insights instrumentation key constant.
-        /// </summary>
-        public const string AppInsightsInstrumentationKeyConstant = "AppInsights-InstrumentationKey";
-
-        /// <summary>
         /// The managed identity client id constant.
         /// </summary>
         public const string ManagedIdentityClientIdConstant = "ManagedIdentityClientId";
@@ -63,29 +38,9 @@ namespace IBBS.AI.Shared.Constants
         public const string IbbsAIAppConfigKeyConstant = "IBBS.AI";
 
         /// <summary>
-        /// The tenant id constant.
-        /// </summary>
-        public const string TenantIdConstant = "TenantId";
-
-        /// <summary>
         /// The client id constant.
         /// </summary>
         public const string ClientIdConstant = "ClientId";
-
-        /// <summary>
-        /// The client secret constant.
-        /// </summary>
-        public const string ClientSecretConstant = "ClientSecret";
-
-        /// <summary>
-        /// The azure ad token request url constant.
-        /// </summary>
-        public const string AzureAdTokenRequestUrlConstant = "AzureAdUrl";
-
-        /// <summary>
-        /// The resource constant.
-        /// </summary>
-        public const string ResourceConstant = "api://{0}/.default";
 
         /// <summary>
         /// The bearer constant.
@@ -101,5 +56,15 @@ namespace IBBS.AI.Shared.Constants
         /// The local appsetings file name.
         /// </summary>
         public const string LocalAppsetingsFileName = "appsettings.development.json";
+
+        /// <summary>
+        /// The auth0 domain constant.
+        /// </summary>
+        public const string Auth0DomainConstant = "Auth0:Domain";
+
+        /// <summary>
+        /// The audience constant.
+        /// </summary>
+        public const string Auth0AudienceConstant = "Auth0:Audience";
     }
 }
